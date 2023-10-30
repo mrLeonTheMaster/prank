491bbcc2e69d96b70a7ae6c1e15d9815e0de963b
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2023-10-30 19:21:23 +0100

    Update spammer.sh

--- spammer.sh
+++ spammer.sh
@@ -1,6 +1,8 @@
 zenity --info --text="Nie naciskaj OK!" --title="Nie masz wyjścia"
 
 zenity --progress --pulsate --no-cancel --text="Crashowanie komputera..." &
+zenity --error --text="Nie zamkniesz mnie!" --title="HAHAHA" &
+
 sleep 5s
 
 while :
